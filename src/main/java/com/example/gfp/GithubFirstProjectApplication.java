1786382fff02af94448a9a802bd8f633097efca6
GouravRajak45 <rajakgourav925@gmail.com>
2024-12-06 00:06:27 +0530

    Project Created

---
+++ src/main/java/com/example/gfp/GithubFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubFirstProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gfp/GithubFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
